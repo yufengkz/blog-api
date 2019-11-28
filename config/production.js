4d4494460ef8b69dce7afbd6eedd6f115433abab
yufengkz <65370392@qq.com>
2019-11-28 12:06:41 +0800

    init

--- config/production.js
+++ config/production.js
@@ -2,5 +2,10 @@ export default {
     env: 'production',
     PORT: 3000,
     HOST: '127.0.0.1',
-    
+    DB_NAME: 'blogs',
+    ROOT: 'root',
+    PASSWORD: '12345678',
+
+    TOKEN_KEY: '123',
+    TOKEN_EXPIRESIN: '24h', //有效期
 }
